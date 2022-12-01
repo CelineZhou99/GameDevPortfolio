6882b04daa8f33ce6a8e214046ea5d070893f045
Celine Zhou <71369583+CelineZhou99@users.noreply.github.com>
2022-12-01 16:22:36 +1300

    feat: added stats and hobbies section, added multiple images/svgs

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,6 +6,9 @@ import {
     HobbiesItemsContainer, 
     Picture,
     NameTag, 
+    StatsItemsContainer,
+    StatsLevel,
+    HobbiesItem,
 } from "./ProfileCss";
 
 function Profile() {
@@ -16,17 +19,34 @@ function Profile() {
             <NameTag>Celine Zhou</NameTag>
         </PictureContainer>
         <StatsContainer>
-            stats
+            <StatsItemsContainer>
+                <p>Work Exp:</p>
+                <StatsLevel src="./LevelFour.svg" alt="level 4" />
+            </StatsItemsContainer>
+            <StatsItemsContainer>
+                <p>Type: Game Developer</p>
+            </StatsItemsContainer>
+            <StatsItemsContainer>
+                <p>Weapon of Choice: Unreal Engine</p>
+            </StatsItemsContainer>
+            <StatsItemsContainer>
+                <p>Game Dev Exp:</p>
+                <StatsLevel src="./LevelFive.svg" alt="level 5" />
+            </StatsItemsContainer>
         </StatsContainer>
         <HobbiesContainer>
             <HobbiesItemsContainer>
-                item
+                <HobbiesItem src="./Likes.svg" alt="Likes" />
             </HobbiesItemsContainer>
             <HobbiesItemsContainer>
-                item
+                <HobbiesItem src="./Cat.svg" alt="Cat" />
+                <HobbiesItem src="./Games.svg" alt="Games" />
+                <HobbiesItem src="./Travel.svg" alt="Travel" />
             </HobbiesItemsContainer>
             <HobbiesItemsContainer>
-                item
+                <HobbiesItem src="./Cooking.svg" alt="Cooking" />
+                <HobbiesItem src="./Dancing.svg" alt="Dancing" />
+                <HobbiesItem src="./Anime.svg" alt="Anime" />
             </HobbiesItemsContainer>
         </HobbiesContainer>
     </Container>
--- src/components/Profile/ProfileCss.js
+++ src/components/Profile/ProfileCss.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// whole container
 export const Container = styled.div`
     background-color: #FFC79C;
     border: solid 10px #B8620E;
@@ -7,43 +8,24 @@ export const Container = styled.div`
     width: 85%;
     height: 450px;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
     margin-top: 100px;
     color: #531403;
     font-size: 20pt;
     font-family: PixelRegular;
 `;
 
+// left container
 export const PictureContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 30%;
-`;
-
-export const StatsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-`;
-
-export const HobbiesContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-`;
-
-export const HobbiesItemsContainer = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
+    width: 25%;
 `;
 
 export const Picture = styled.img`
-    width: 48%;
+    width: 60%;
     height: 70%;
     border: solid 10px #B8620E;
     border-radius: 10px;
@@ -56,4 +38,47 @@ export const NameTag = styled.div`
     background-color: #FED283;
     text-align: center;
     padding: 10px;
+`;
+
+// middle container
+export const StatsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: left;
+    border-left: solid 3px #C29878;
+    border-right: solid 3px #C29878;
+    padding-left: 2%;
+    width: 35%;
+`;
+
+export const StatsItemsContainer = styled.div`
+    display: flex;
+    text-align: left;
+`;
+
+export const StatsLevel = styled.img`
+    width: 35%;
+    margin-left: 10px;
+`;
+
+// right container
+export const HobbiesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    padding-left: 1%;
+`;
+
+export const HobbiesItemsContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 80%;
+`;
+
+export const HobbiesItem = styled.img`
+    margin: 10px; 
+    width: 30%;
 `;
