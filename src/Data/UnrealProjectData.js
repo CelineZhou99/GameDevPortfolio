ae3d99c6d2798fed3707ca3136885be8ab572e2e
Celine Zhou <71369583+CelineZhou99@users.noreply.github.com>
2022-12-28 17:28:33 +1100

    fix: updated spelling

--- src/Data/UnrealProjectData.js
+++ src/Data/UnrealProjectData.js
@@ -1,19 +1,19 @@
-const FPSDemo = "https://www.youtube.com/embed/Ca3wOfvwKpA";
+const ShooterDemo = "https://www.youtube.com/embed/Ca3wOfvwKpA";
 const TankDemo = "https://www.youtube.com/embed/bl45NCqkt3U"
 const DungeonDemo = "https://www.youtube.com/embed/w6QSgaJhm4g"
 
-const FPSDescription = "Emerce yourself in a Sci-Fi themed First Person Shooter game packed with action and intense music. Hone your aiming skills and take down the enemies, but be aware - they will search and chase after you with all their might! (Full Controller Support)"
+const ShooterDescription = "Emerce yourself in a Sci-Fi themed Third Person Shooter game packed with action and intense music. Hone your aiming skills and take down the enemies, but be aware - they will search and chase after you with all their might! (Full Controller Support)"
 const TankDescription = "Control a mighty toon tank and challenge yourself to beat the many enemies wired to destroy you when you get too close. Warning: the battle will be so intense the ground will start shaking, so hold onto your hats!"
 const DungeonDescription = "Hop into the shoes of a professional thief and sneak into the gruesome dungeon and steal the golden statue. There will be many puzzles along the way to stop you in your tracks. But nothing's stopping that big brain of yours!"
 
 export const UnrealProjects = [
 	{
 		isFlipped: false,
-		vidSrc: FPSDemo, 
+		vidSrc: ShooterDemo, 
 		title: "Sci-Fi Shooter", 
 		gitLink: "https://github.com/CelineZhou99/SciFiShooter-UE5", 
 		engine: "Unreal Engine 5", 
-		description: FPSDescription, 
+		description: ShooterDescription, 
 		number: "01"
 	},
 	{
